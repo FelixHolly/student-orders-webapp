f6f276eac08b10f01feec5ff81fd0fdf8f1d125e
Felix <felixhollndonner@hotmail.com>
2025-12-07 14:26:19 -0700

    added student list component

---
+++ src/app/components/student-list/student-list.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit, Output, EventEmitter, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Student } from '../../models/student.model';
+import { StudentService } from '../../services/student.service';
+
+@Component({
+  selector: 'app-student-list',
+  imports: [CommonModule],
+  templateUrl: './student-list.component.html',
+  styleUrl: './student-list.component.scss'
+})
+export class StudentListComponent implements OnInit {
+  @Output() studentSelected = new EventEmitter<Student>();
+
+  students = signal<Student[]>([]);
+  selectedStudent = signal<Student | null>(null);
+  loading = signal<boolean>(false);
+  errorMessage = signal<string>('');
+
+  constructor(private studentService: StudentService) {}
+
+  ngOnInit(): void {
+    this.loadStudents();
+  }
+
+  loadStudents(): void {
+    this.loading.set(true);
+    this.errorMessage.set('');
+
+    this.studentService.getAll().subscribe({
+      next: (students) => {
+        this.students.set(students);
+        this.loading.set(false);
+      },
+      error: (err) => {
+        this.errorMessage.set(err.error?.message || 'Failed to load students');
+        this.loading.set(false);
+      }
+    });
+  }
+
+  selectStudent(student: Student): void {
+    this.selectedStudent.set(student);
+    this.studentSelected.emit(student);
+  }
+
+  isSelected(student: Student): boolean {
+    return this.selectedStudent()?.id === student.id;
+  }
+
+  onStudentCreated(student: Student): void {
+    this.students.update(students => [...students, student]);
+  }
+}
